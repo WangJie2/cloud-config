b43067ac746d5363753d81ae3934694768d15a2b
王杰 <1054918167@qq.com>
2018-10-07 19:32:04 +0800

    消息总线

--- config-client/src/main/java/com/wj/cloud/config/controller/TestController.java
+++ config-client/src/main/java/com/wj/cloud/config/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wj.cloud.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by WangJie on 2018/9/29.
  */
 @RestController
+@RefreshScope
 public class TestController {
     @Value("${foo}")
     String foo;
